c4dd3369e2f465c471ae8c42014a7cb941264422
Rehan Mahmood <rehanone@gmail.com>
2016-05-22 00:02:47 -0400

    Test fixes

--- spec/classes/params_spec.rb
+++ spec/classes/params_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe 'cfssl::params', :type => :class do
 
-  it { is_expected.to contain_cfssl__params }
-
   it "Should not contain any resources" do
     expect(subject.call.resources.size).to eq(4)
   end
