76e74b4ef02aed4af255c4770a1646d5667ec4aa
Rehan Mahmood <rehanone@gmail.com>
2016-05-21 23:59:56 -0400

    Test fixes

--- spec/classes/cfssl_spec.rb
+++ spec/classes/cfssl_spec.rb
@@ -11,8 +11,8 @@ describe 'cfssl' do
 
   context 'with default values for all parameters' do
     it { should contain_class('cfssl::install') }
-    it { should contain_class('cfssl::config').that_requires('cfssl::install') }
-    it { should contain_class('cfssl::service').that_subscribes_to('cfssl::config') }
-    it { should contain_class('cfssl::firewall').that_requires('cfssl::service') }
+    it { should contain_class('cfssl::config') }
+    it { should contain_class('cfssl::service') }
+    it { should contain_class('cfssl::firewall') }
   end
 end
