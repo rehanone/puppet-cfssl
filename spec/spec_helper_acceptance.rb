5cc38672d50f9fd28e771dbabc42b9b58e7b3e3d
Rehan Mahmood <rehanone@gmail.com>
2017-03-25 13:20:55 -0400

    Upgrade rehan-wget version to 1.1.2

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -12,7 +12,7 @@ unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
     environmentpath = environmentpath.split(':').first if environmentpath
 
     on host, puppet('module install puppetlabs-stdlib --version 4.16.0')
-    on host, puppet('module install rehan-wget --version 1.1.1')
+    on host, puppet('module install rehan-wget --version 1.1.2')
   end
 end
 
