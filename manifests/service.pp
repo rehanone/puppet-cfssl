8c15daa250510fc11d93e9eacf35b6cb2ecb0da3
Rehan Mahmood <rehanone@gmail.com>
2019-01-02 00:09:02 -0500

    - Updated `pdk` templates.

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,6 +4,11 @@ class cfssl::service () inherits cfssl {
 
   assert_private("Use of private class ${name} by ${caller_module_name}")
 
+  $service_provider = $facts['service_provider'] ? {
+    'debian' => 'systemd',
+    default  => $facts['service_provider'],
+  }
+
   if $cfssl::service_manage {
 
     $service_attributes = {
@@ -15,7 +20,7 @@ class cfssl::service () inherits cfssl {
       'intermediate_ca_id' => $cfssl::intermediate_ca_id,
     }
 
-    case $::service_provider {
+    case $service_provider {
       'upstart': {
         file { "add-${cfssl::service_name}-conf":
           ensure  => file,
@@ -26,7 +31,7 @@ class cfssl::service () inherits cfssl {
           content => epp("${module_name}/service/upstart/cfssl.conf.epp", $service_attributes),
         }
       }
-      'systemd': {
+      'systemd', 'debian': {
         file { "remove-${cfssl::service_name}-conf":
           ensure => absent,
           path   => "/etc/init/${cfssl::service_name}.conf",
@@ -51,14 +56,14 @@ class cfssl::service () inherits cfssl {
           content => epp("${module_name}/service/redhat/cfssl.epp", $service_attributes),
         }
       }
-      default: { }
+      default: {}
     }
 
     service { $cfssl::service_name:
       ensure    => $cfssl::service_ensure,
       enable    => $cfssl::service_enable,
       name      => $cfssl::service_name,
-      provider  => $::service_provider,
+      provider  => $service_provider,
       subscribe => [
         Class["${module_name}::config"],
         File["add-${cfssl::service_name}-conf"]
